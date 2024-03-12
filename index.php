6f697bb3d5ec67b901125ce748b06c3b5b6ed015
Samiul H Pranto <samihoq10@gmail.com>
2024-03-12 15:12:50 +0600

    Styling custom post type css

--- index.php
+++ index.php
@@ -71,8 +71,9 @@ wp_nav_menu( [
 <!-- Custom Post Dynamic  -->
 
 <div class="posts">
-    <h3>Holy Ramadan Month Starts Today</h3>
+    <h2>Holy Ramadan Month Starts Today</h2>
     <img src="<?php echo get_template_directory_uri();?>/images/using laptop.jpg" alt="">
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic sapiente, dolorum facilis ut, nobis voluptatibus sint debitis saepe vitae cupiditate animi ratione eum ducimus recusandae? Voluptates autem hic eveniet sequi eius perspiciatis consequatur tempore voluptatem. Praesentium ex ut totam, autem, qui doloribus et odio unde illo nesciunt ab saepe tempora, non quae sint optio reprehenderit aut. Ratione fugit, illo deserunt tempora pariatur blanditiis corporis. Cum aliquam ipsum expedita consequuntur libero deserunt! Aperiam recusandae voluptatum hic magnam et porro provident commodi beatae maiores. Laudantium, magnam nisi? Quis, repudiandae suscipit sed illum quo, inventore aliquid officiis numquam, totam id aut nobis nemo?</p>
 </div>
 
 
